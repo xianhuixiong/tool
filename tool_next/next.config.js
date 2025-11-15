d747b93c3a7cb5a6f3cb31136e3f20bfbb58dfd0
xianhuixiong <141015951+xianhuixiong@users.noreply.github.com>
2025-11-15 14:22:43 +0800

    Delete tool_next directory

--- tool_next/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enable React strict mode for highlighting potential problems
-  reactStrictMode: true
-};
-
-module.exports = nextConfig;
